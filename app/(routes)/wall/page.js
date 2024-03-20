87f57bea5ae1d0815226bd6f1b161a4bcf323bda
camcgreen <c.c.green@outlook.com>
2024-03-20 11:14:34 +0000

    Temp grid style for wall page

--- app/(routes)/wall/page.js
+++ app/(routes)/wall/page.js
@@ -11,27 +11,41 @@ export default function Mosaic() {
       const imagesTmp = []
       snapshot.forEach((doc) => {
         imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
+        imagesTmp.push(doc.data().img)
       })
       setImages(imagesTmp)
     })
     return () => unsubscribe()
   }, [])
   return (
-    <main>
-      <h1>Images</h1>
-      <ul>
-        {images.map((img, i) => {
-          return (
-            <li key={i}>
-              <img
-                src={img}
-                alt={`Image ${i}`}
-                className='aspect-square object-cover w-32'
-              />
-            </li>
-          )
-        })}
-      </ul>
-    </main>
+    <div className='h-screen-sm flex justify-center items-center'>
+      <header className='absolute left-0 top-0 p-8'>
+        <img src='/images/logo.svg' alt='Logo' className='w-12 aspect-square' />
+      </header>
+      <main>
+        <ul className='grid grid-cols-12 mb-10'>
+          {images.map((img, i) => {
+            return (
+              <li key={i}>
+                <img
+                  src={img}
+                  alt={`Image ${i}`}
+                  className='aspect-square object-cover w-32'
+                />
+              </li>
+            )
+          })}
+        </ul>
+        <h1 className='text-4xl font-medium text-center'>
+          FACES OF <span className='font-extralight'>BRAND</span>
+        </h1>
+      </main>
+    </div>
   )
 }
